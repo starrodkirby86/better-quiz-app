fe713cf76360465a64c9092baf16b4a3a7637add
Nick Morley <nick.w.morley@gmail.com>
2015-11-12 15:43:19 -0800

    Replace Lost Code from 6AM brain fart lost merge
    --> move core -> loading
    --> mulitple ans fix
    --> options screen
    --> title screen
    --> loading scene
    --> Label Options UI

--- Assets/Scripts/GUI/GameOverAgent.cs
+++ Assets/Scripts/GUI/GameOverAgent.cs
@@ -28,10 +28,7 @@ public class GameOverAgent : MonoBehaviour {
 		foreach(Results i in myCore.myResults)
 		{
 			resultsMessage += "Question " + (questionValue) + "\n";
-<<<<<<< HEAD
 			resultsMessage += "=================\n";
-=======
->>>>>>> refs/remotes/origin/master
 			for(int j = 0; j < myCore.players.Count; ++j)
 			{
 				resultsMessage += "Player " + i.players[j].playerName + ": ";
